52fe806348613e0bfd2e84b474166d457142bb8c
CharilLiu <100350596+CharilLiu@users.noreply.github.com>
2026-06-12 00:23:03 +0800

    JavaSE/day02_变量_类型转换_运算符/Test8.java

---
+++ JavaSE/day02_变量_类型转换_运算符/Test8.java
@@ -0,0 +1,8 @@
+public class Test8 {
+    public static void main(String[] args) {
+        int x = 123, y = 456, z = 234;
+        int max = x > y ? x : y;
+        max = max > z ? max : z;
+        System.out.println("三个数中的最大值是：" + max);
+    }
+}

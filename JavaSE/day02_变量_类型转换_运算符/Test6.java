47988f5b5b87d9001e12ec6a037163801e252fe0
CharilLiu <100350596+CharilLiu@users.noreply.github.com>
2026-06-12 00:17:25 +0800

    JavaSE/day02_变量_类型转换_运算符/Test6.java

---
+++ JavaSE/day02_变量_类型转换_运算符/Test6.java
@@ -0,0 +1,8 @@
+public class Test6 {
+    public static void main(String[] args) {
+        int hours = 89;
+        int day = 89 / 24;
+        int hour = 89 % 24;
+        System.out.println(hours + "小时等于" + day + "天" + hour + "小时");
+    }
+}
